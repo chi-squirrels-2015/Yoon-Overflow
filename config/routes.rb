9434e76dd890e8230777107570adcd2f1d268c3f
thejdavid <jdlavaud@gmail.com>
2015-02-26 16:05:21 -0600

    basic user profil finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   get '/users/:id', to: 'users#show'
+
+  resources :questions do
+    resources :answers, except: [:index, :show]
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
