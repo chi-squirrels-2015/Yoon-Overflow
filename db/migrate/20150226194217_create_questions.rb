2e3fae1453ee1b6fe9c1480e609cfd6e3dbb189a
Elliott Young <elliott.a.young@gmail.com>
2015-02-26 13:58:47 -0600

    Add title column to question table, change content to t.text

--- db/migrate/20150226194217_create_questions.rb
+++ db/migrate/20150226194217_create_questions.rb
@@ -1,7 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-      t.string :content
+      t.string     :title
+      t.text       :content
       t.belongs_to :user
 
       t.timestamps
