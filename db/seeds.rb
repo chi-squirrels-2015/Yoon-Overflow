1174cb18f2856ccfc9d39b820da07b5a6c702559
Elliott Young <elliott.a.young@gmail.com>
2015-02-26 14:06:08 -0600

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,21 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 users = []
 5.times do
-  User.create(name: Faker::Name.)
+  users << User.create(name: Faker::Name.name, email: Faker::Internet.email,
+                       password: "password", bio: Faker::Lorem.paragraph,
+                       avatar_url: Faker::Avatar.image)
+end
+
+questions = []
+10.times do
+  questions << Question.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph,
+                               user: users.sample)
+end
+
+answers = []
+questions.each do |question|
+  answers << Answer.create(content: Faker::Hacker.say_something_smart, question: question,
+                           user: users.sample)
+  answers << Answer.create(content: Faker::Hacker.say_something_smart, question: question,
+                           user: users.sample)
 end
