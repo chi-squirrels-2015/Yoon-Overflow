62971c643573c0b7760d0ece741a30206edd63fb
Elliott Young <elliott.a.young@gmail.com>
2015-02-26 15:19:56 -0600

    Add validations on questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,9 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many   :answers
   has_many   :votes, as: :votable
+
+  validates :title,   presence: true,
+    inclusion: { message: "Title is a required field" }
+  validates :content, presence: true,
+    inclusion: { message: "Your question needs to have content" }
 end
