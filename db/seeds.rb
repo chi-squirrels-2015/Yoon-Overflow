c406ad0a7c2daee2bd76913bd5ab35ab0c740efb
Elliott Young <elliott.a.young@gmail.com>
2015-02-26 13:54:16 -0600

    Create initial database and migrate in create tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+users = []
+5.times do
+  User.create(name: Faker::Name.)
+end
