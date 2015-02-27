ffaf5744bf6b38b6d7f51247c00d272435698e3f
janeskim <kim.jane.sue@gmail.com>
2015-02-27 11:19:22 -0600

    added upvote and downvote links to static views/index

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,13 +2,15 @@ class VotesController < ApplicationController
 
   def upvote
     @question = Question.find(params[:question_id])
+    @user = current_user
     @question.upvote_by current_user
     redirect_to question_path(@question)
   end
 
   def downvote
     @question = Question.find(params[:id])
-    @question.upvote_by current_user
+    @user = current_user
+    @question.downvote_by current_user
     redirect_to question_path(@question)
   end
 
