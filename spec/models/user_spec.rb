275a0b7dd56291f4a83a20c9bb3128bef3ea3ef8
Elliott Young <elliott.a.young@gmail.com>
2015-02-27 10:29:14 -0600

    Write tests for password validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,18 +21,28 @@ describe User do
 
       it 'should accept a properly formatted, unique email' do
         user.email = "fake@working.org"
+        user.save
         expect(user.errors[:email]).to be_empty
       end
     end
 
     context 'password' do
       it 'should not accept a password with fewer than 8 characters' do
+        user.password = "break"
+        user.save
+        expect(user.errors[:password]).to_not be_empty
       end
 
       it 'should not allow password to be blank' do
+        user.password = ""
+        user.save
+        expect(user.errors[:password]).to_not be_empty
       end
 
       it 'should accept a password of 8 or more characters' do
+        user.password = "working1"
+        user.save
+        expect(user.errors[:password]).to be_empty
       end
     end
   end
