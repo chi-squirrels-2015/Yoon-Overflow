d9efe5e4ec3c6e2b083aa46c7dcbd5d526ed7bad
janeskim <kim.jane.sue@gmail.com>
2015-02-28 10:48:05 -0600

    can properly create comments for questions only

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,18 @@
 class CommentsController < ApplicationController
   def new
+    @question = Question.find(params[:question_id])
     @comment = Comment.new
   end
 
   def create
+    @question = Question.find(params[:question_id])
     @comment = Comment.new(comment_params)
     @comment.author = User.find(current_user.id)
+    @comment.commentable_id = @question.id
+    @comment.commentable_type = "Question"
     if @comment.save
-      render :show, layout: false
+      # render :show, layout: false
+      redirect_to question_path(@question)
     else
       @error = @comment.errors
       render :new
