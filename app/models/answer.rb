691b3690e1fbcadb1cfd6a77bf595fa027f97c18
Elliott Young <elliott.a.young@gmail.com>
2015-02-28 07:33:32 -0600

    Add validations and associations on comments

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,6 +4,7 @@ class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
   has_many   :votes, as: :votable
+  has_many :comments, as: :commentable
 
   validates :content, presence: true
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,7 @@
 class Comment < ActiveRecord::Base
+  validates :content, presence: true
+  validates :author, presence: true
+
+  belongs_to :author, class_name: "User"
+  belongs_to :commentable, polymorphic: true
 end
