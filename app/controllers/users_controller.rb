eb0e1ce40a5e0c0bc5f0e1fa2c93b4c57d1e1bbf
thejdavid <jdlavaud@gmail.com>
2015-02-28 08:34:37 -0600

    more bootstrap up and down arrow left

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
-class UsersController < ActionController::Base
+class UsersController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def show
   	# @user = User.find(params[:id])
   	@user = User.friendly.find(params[:id])
+  	# @test = @user.questions.order(:timestamps) 
+      	@test = @user.questions
+      	
   end
 end
