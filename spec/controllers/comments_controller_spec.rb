5b73824e28ca6af1b31a2955b8e1c89912c57514
Elliott Young <elliott.a.young@gmail.com>
2015-02-28 08:13:29 -0600

    Add skeleton for Comment controller testing

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,4 +1,38 @@
 require 'rails_helper'
 
 describe CommentsController do
+  let!(:user)     { User.create( name: "fake", email: "fake@fake.org", password: "password" ) }
+  let!(:question) { Question.create( title: "Test Question", content: "This is a question",
+                                     user: user ) }
+  let!(:answer)   { Answer.create( content: "This is an Answer", user: user, question: question ) }
+  let!(:commentQ)  { Comment.create( content: "This is a Comment", commentable_type: "Question",
+                                     commentable_id: question.id ) }
+  let!(:commentA)  { Comment.create( content: "This is a Comment", commentable_type: "Answer",
+                                     commentable_id: answer.id ) }
+
+  context "Commenting on a Question" do
+    describe "GET #new" do
+    end
+
+    describe "POST #create" do
+      context 'when invalid params are given' do
+      end
+
+      context 'when valid params are given' do
+      end
+    end
+  end
+
+  context "Commenting on an Answer" do
+    describe "GET #new" do
+    end
+
+    describe "POST #create" do
+      context 'when invalid params are given' do
+      end
+
+      context 'when valid params are given' do
+      end
+    end
+  end
 end
