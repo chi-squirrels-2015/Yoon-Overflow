af4733528f62d28204ac3d21816290057ab64b0b
thejdavid <jdlavaud@gmail.com>
2015-03-01 15:25:54 -0600

    adding img field on registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,7 @@ class RegistrationsController < Devise::RegistrationsController
   private
  
   def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email,:avatar_url , :password, :password_confirmation)
   end
  
   def account_update_params
