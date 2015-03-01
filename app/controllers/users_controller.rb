813eb03fee1ee7856b471d03a5b64878f77f3f39
Elliott Young <elliott.a.young@gmail.com>
2015-03-01 13:31:25 -0600

    Add update method, edit function working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,21 @@ class UsersController < ApplicationController
   def edit
     @user = User.friendly.find(params[:id])
   end
+
+  def update
+    @user = User.friendly.find(params[:id])
+    @user.update(user_params)
+
+    if @user.save
+      redirect_to @user, notice: "Your Profile was successfully updated. :D"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :avatar_url, :bio)
+  end
 end
